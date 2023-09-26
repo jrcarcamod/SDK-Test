48cad92806c9daf8d915beecb99a3706e2224eea
Jose Carcamo <jose.carcamo@digitalreef.com>
2023-09-26 17:01:26 -0600

    eliminar publicacion core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -47,16 +47,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("all") {
-                from(components["release"])
-                groupId = "com.github.jrcarcamod"
-                version = "1.0"
-                artifactId = "core"
-            }
-        }
-    }
-}
