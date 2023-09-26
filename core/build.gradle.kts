d62a8e4986922558a28af003c66311447bf84ed8
Jose Carcamo <jose.carcamo@digitalreef.com>
2023-09-26 14:50:46 -0600

    add afterEvaluate core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -42,3 +42,16 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("all") {
+                from(components["release"])
+                groupId = "com.github.jrcarcamod"
+                version = "1.0"
+                artifactId = "core"
+            }
+        }
+    }
+}
