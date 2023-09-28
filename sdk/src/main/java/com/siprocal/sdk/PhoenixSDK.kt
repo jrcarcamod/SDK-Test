68eade3918a6f4fbe7e03a33aaf2ffcabaa1ac15
Jose Carcamo <jose.carcamo@digitalreef.com>
2023-09-28 12:22:20 -0600

    cambio en la clase

--- sdk/src/main/java/com/siprocal/sdk/PhoenixSDK.kt
+++ sdk/src/main/java/com/siprocal/sdk/PhoenixSDK.kt
@@ -6,15 +6,21 @@ import androidx.annotation.Keep
 import com.siprocal.core.IAppInsight
 import com.siprocal.core.IMobileProfile
 import com.siprocal.core.ITelephonyProfile
+class PhoenixSDK private constructor(
+    private val context: Context,
+    private val appInsight: IAppInsight? = null,
+    private val telephonyProfile: ITelephonyProfile? = null,
+    private val mobileProfile: IMobileProfile? = null
+) {
+    companion object {
+        fun init(context: Context, initBlock: PhoenixSDK.() -> Unit) {
+            val sdk = PhoenixSDK(context)
+            sdk.initBlock()
+            sdk.internalInit()
+        }
+    }
 
-object PhoenixSDK {
-    @JvmStatic
-    fun init(
-        context: Context,
-        appInsight: IAppInsight? = null,
-        telephonyProfile: ITelephonyProfile? = null,
-        mobileProfile: IMobileProfile? = null
-    ) {
+    private fun internalInit() {
         appInsight?.getAppInsight(context)
         telephonyProfile?.getTelephonyProfile()
         mobileProfile?.getMobileProfile(context)
